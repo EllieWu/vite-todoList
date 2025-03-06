8d3a0b5e49039314df71d32eda24c4475a0e79c9
elliewu <a0981262519@gmail.com>
2025-03-06 19:59:53 +0800

    add base

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base:'./',
+  base:'/vite-todoList',
   plugins: [vue(), tailwindcss()],
   css: {
     preprocessorOptions: {
